5e97adb6074e9a01f9e93aea84f3c48061dc458e
dorianzheng <xingzhengde72@gmail.com>
2026-02-28 11:31:21 +0800

    refactor(build): simplify build script by removing runtime copy logic

--- sdks/python/build.rs
+++ sdks/python/build.rs
@@ -1,7 +1,1 @@
-fn main() {
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/runtime");
-
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/runtime");
-}
+fn main() {}
